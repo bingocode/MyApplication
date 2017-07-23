64c8d8679a3907d5bc634779efb82841a79f5040
Bingo <2416940975@qq.com>
2017-07-23 15:36:18 +0800

    add item

--- app/src/main/java/com/example/admin/fisrtdemo/model/Item.kt
+++ app/src/main/java/com/example/admin/fisrtdemo/model/Item.kt
@@ -4,6 +4,6 @@ package com.example.admin.fisrtdemo.model
  * Created by admin on 2017/7/16.
  */
 fun getItems():List<Item> {
-    return (1..10).map { Item(it.toString()) }
+    return (1..100).map { Item(it.toString()) }
 }
 data  class  Item(val title: String)
