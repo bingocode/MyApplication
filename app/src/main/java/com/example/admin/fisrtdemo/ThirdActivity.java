4e45254c234390bf6fe0cee99c24971dd84124b6
zengbin <2416940975@qq.com>
2018-08-07 13:44:04 +0800

    update 3d

--- app/src/main/java/com/example/admin/fisrtdemo/ThirdActivity.java
+++ app/src/main/java/com/example/admin/fisrtdemo/ThirdActivity.java
@@ -3,8 +3,12 @@ package com.example.admin.fisrtdemo;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
+    private TextView mbacktv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +18,12 @@ public class ThirdActivity extends AppCompatActivity {
         if(actionBar !=null){
             actionBar.hide();
         }
+
+        mbacktv = (TextView) findViewById(R.id.backorigion);
+        mbacktv.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Toast.makeText(ThirdActivity.this, "clickbackOrigenbtn", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
