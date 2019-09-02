8a6c686e22bc73c039ce3f87bb99afdcb42d1680
zengbin <2416940975@qq.com>
2019-09-02 11:06:24 +0800

    feat: 增加自定义switch

---
+++ app/src/main/java/com/example/admin/fisrtdemo/page/PageActivity.java
@@ -0,0 +1,14 @@
+package com.example.admin.fisrtdemo.page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.admin.fisrtdemo.R;
+
+public class PageActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_page);
+  }
+}
